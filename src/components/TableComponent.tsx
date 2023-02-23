dcb154ddfea7f7bc6b30ba83aa3f8a7ab2eea31a
David Ye <dly8@case.edu>
2023-02-22 21:03:22 -0500

    working on updating npm

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,7 +1,8 @@
-import { SimpleGrid, Space } from "@mantine/core";
+import { SimpleGrid, Button } from "@mantine/core";
 import { DataTable, DataTableSortStatus } from "mantine-datatable";
 import { ReactElement, useEffect, useState } from "react";
 import sortBy from "lodash/sortBy";
+import { exportToExcel } from 'react-json-to-excel';
 import _dataset from "../Uncountable Front End Dataset.json";
 import { ProcessedData } from "../App";
 
@@ -24,58 +25,63 @@ export function TableComponent(props: TableProps): ReactElement {
   }, [sortStatus]);
 
   return (
-    <DataTable
-      minHeight={150}
-      columns={[
-        { accessor: "id", title: "Experiment ID" },
-        {
-          accessor: "num",
-          title: "Experiment Number",
-          render: ({ num }) => (num === -1 ? "" : `Experiment ${num}`),
-          sortable: true,
-        },
-        {
-          accessor: "date",
-          render: ({ date }) =>
-            date === undefined ? "N/A" : date.format("MMMM D, YYYY"),
-          sortable: true,
-        },
-      ]}
-      idAccessor="id"
-      records={records}
-      rowExpansion={{
-        allowMultiple: true,
-        content: ({ record }) => (
-          <SimpleGrid cols={2} color="red.2">
-            <DataTable
-              styles={{
-                root: {
-                  color: "red.2",
-                },
-              }}
-              withBorder
-              minHeight={150}
-              columns={[
-                { accessor: "name", title: "Ingredient/Input" },
-                { accessor: "value", title: "Amount" },
-              ]}
-              records={record.inputs.filter((x) => x.value !== 0)}
-            />
-            <DataTable
-              withBorder
-              minHeight={150}
-              columns={[
-                { accessor: "name", title: "Property" },
-                { accessor: "value" },
-              ]}
-              records={record.outputs}
-            />
-          </SimpleGrid>
-        ),
-      }}
-      sortStatus={sortStatus}
-      onSortStatusChange={setSortStatus}
-      textSelectionDisabled
-    />
+    <>
+      <DataTable
+        minHeight={150}
+        columns={[
+          { accessor: "id", title: "Experiment ID" },
+          {
+            accessor: "num",
+            title: "Experiment Number",
+            render: ({ num }) => (num === -1 ? "" : `Experiment ${num}`),
+            sortable: true,
+          },
+          {
+            accessor: "date",
+            render: ({ date }) =>
+              date === undefined ? "N/A" : date.format("MMMM D, YYYY"),
+            sortable: true,
+          },
+        ]}
+        idAccessor="id"
+        records={records}
+        rowExpansion={{
+          allowMultiple: true,
+          content: ({ record }) => (
+            <SimpleGrid cols={2} color="red.2">
+              <DataTable
+                styles={{
+                  root: {
+                    color: "red.2",
+                  },
+                }}
+                withBorder
+                minHeight={150}
+                columns={[
+                  { accessor: "name", title: "Ingredient/Input" },
+                  { accessor: "value", title: "Amount" },
+                ]}
+                records={record.inputs.filter((x) => x.value !== 0)}
+              />
+              <DataTable
+                withBorder
+                minHeight={150}
+                columns={[
+                  { accessor: "name", title: "Property" },
+                  { accessor: "value" },
+                ]}
+                records={record.outputs}
+              />
+            </SimpleGrid>
+          ),
+        }}
+        sortStatus={sortStatus}
+        onSortStatusChange={setSortStatus}
+        textSelectionDisabled
+      />
+      <Button
+        onClick={exportToExcel(table, "a.xlsx")}
+      >Download File</Button>
+    </>
   );
 }
